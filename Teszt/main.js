0d3683cfd1edfbcc2e46d7804a73fbbe938179d1
KeresztesHunor <kereszteshunor36@gmail.com>
2024-01-18 23:19:40 +0100

    localization invariant mode

--- Teszt/main.js
+++ Teszt/main.js
@@ -1,9 +1,23 @@
 $(() => {
+    get("jarmutipusok");
+    get("jarmutipusok/1");
+    get("allomasok");
+    get("allomasok/1");
+    get("vonalak");
+    get("vonalak/1");
+    get("inditasok");
+    get("inditasok/1/1/420");
+    get("megallok");
+    get("megallok/1/2");
+});
+
+function get(url)
+{
     axios
-        .get("https://localhost:7078/jarmutipusok")
+        .get("https://localhost:7078/" + url)
         .then(response => {
             console.log(response.data);
         })
         .catch(console.error)
     ;
-});
+}
