9082303452b8892c4a226a3befb280652b1a75f6
KeresztesHunor <kereszteshunor36@gmail.com>
2024-01-18 21:30:30 +0100

    cors

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,6 +6,18 @@ namespace Backend
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
             builder.Services.AddScoped<AppDbContext, AppDbContext>();
+            builder.Services.AddCors(options => {
+                options.AddDefaultPolicy(policy => {
+                    policy
+                        .WithOrigins(
+                            "http://localhost:5500",
+                            "http://127.0.0.1:5500"
+                        )
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                    ;
+                });
+            });
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -16,6 +28,7 @@ namespace Backend
                 app.UseSwaggerUI();
             }
             app.UseHttpsRedirection();
+            app.UseCors();
             app.UseAuthorization();
             app.MapControllers();
             app.Run();
