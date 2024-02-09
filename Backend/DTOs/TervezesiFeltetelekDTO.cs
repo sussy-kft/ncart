3f9ccad1a158c26bd873fd5d9c407900e8b639e8
malackapite <veress.marcell@protonmail.com>
2024-02-09 16:57:19 +0100

    +datum

--- Backend/DTOs/TervezesiFeltetelekDTO.cs
+++ Backend/DTOs/TervezesiFeltetelekDTO.cs
@@ -8,6 +8,7 @@ namespace Backend.DTOs
         [Required] public int hova { get; set; }
         [Required] public short mikor { get; set; }
         [Required] public bool indulas_e { get; set; }
+        public string datum { get; set; }
         public List<int> jarmuKivetel { get; set; }
         public List<string> vonalKivetel { get; set; }
     }
