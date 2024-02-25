5ae2e3a59226192dd976d1c3fc282dc396ac66d1
malackapite <veress.marcell@protonmail.com>
2024-02-25 02:34:02 +0100

    refactor 🏭 +post, patch

--- Frontend/admin/src/context/AxiosContext.js
+++ Frontend/admin/src/context/AxiosContext.js
@@ -9,9 +9,10 @@ export const AxiosContext = createContext();
 export const AxiosProvider = ({ children }) => {
     const [axiosId, setAxiosId] = React.useState(Math.random());
     const {addInfoPanel} = React.useContext(InfoPanelContext);
+    const baseUrl = "https://localhost:44339/";
 
     const get = (url, keys, callback, errorCallback) => {
-        axios.get("https://localhost:44339/" + url)
+        axios.get(baseUrl + url)
         .then(response => {
             callback(response.data);
         })
@@ -21,7 +22,7 @@ export const AxiosProvider = ({ children }) => {
     }
 
     const destroy = (url, id) => {
-        axios.delete("https://localhost:44339/" + url + "/" )
+        axios.delete(baseUrl + url + "/" + id)
         .then(response => {
             console.log(response.data);
             console.log(response);
@@ -33,9 +34,37 @@ export const AxiosProvider = ({ children }) => {
             addInfoPanel(<InfoPanel bg={"danger"} text={error.message}/>);
         })
     }
+
+    const post = (url, item) => {
+        axios.post(baseUrl + url, item)
+        .then(response => {
+            console.log(response.data);
+            console.log(response);
+            setAxiosId(Math.random());
+            addInfoPanel(<InfoPanel bg={"success"} text={"A frissítés sikeres volt!"}/>);
+        })
+        .catch(error => {
+            //error.code
+            addInfoPanel(<InfoPanel bg={"danger"} text={error.message}/>);
+        })
+    }
+
+    const patch = (url, id, item) => {
+        axios.patch(baseUrl + url + "/" + id, item)
+        .then(response => {
+            console.log(response.data);
+            console.log(response);
+            setAxiosId(Math.random());
+            addInfoPanel(<InfoPanel bg={"success"} text={"A frissítés sikeres volt!"}/>);
+        })
+        .catch(error => {
+            //error.code
+            addInfoPanel(<InfoPanel bg={"danger"} text={error.message}/>);
+        })
+    }
     
     return (
-        <AxiosContext.Provider value={{axiosId, get, destroy}}>
+        <AxiosContext.Provider value={{axiosId, get, destroy, post, patch}}>
             {children}  
         </AxiosContext.Provider>
     );
--- Frontend/admin/src/komponensek/Megallok.js
+++ Frontend/admin/src/komponensek/Megallok.js
@@ -1,9 +1,16 @@
+import React, { useContext } from 'react';
+import { InfoPanelContext } from '../context/InfoPanelContext';
+import { ToastContainer } from 'react-bootstrap';
+
 function Megallok()
 {
+    const {InfoPanels} = useContext(InfoPanelContext);
+
     return (
         <>
             <div>Megállok</div>
             <div>Nincs út</div>
+            <ToastContainer position="top-end" className="position-fixed">{InfoPanels}</ToastContainer>
         </>
     );
 }
