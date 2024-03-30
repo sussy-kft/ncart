2cb8e30be327875f41a1047e42827119cc93c7a7
malackapite <veress.marcell@protonmail.com>
2024-03-30 20:04:23 +0100

    user/admin osszekapcsolasa

--- Frontend/admin/src/App.js
+++ Frontend/admin/src/App.js
@@ -1,18 +1,28 @@
 import Admin from "./Admin";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Bejelentkezes from "./komponensek/Bejelentkezes";
 
 function App() {
+  const location = window.location.pathname;
+
   return (
-    <div className="App" >
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Bejelentkezes />} />
-          <Route path="/admin/*" element={<Admin/>} />
-        </Routes>
-      </Router>
+    <div className="App">
+      {location === "/" ? (
+        <iframe
+          src="http://localhost:42069/index.html"
+          title="External HTML"
+          style={{ width: "100%", height: "100vh" }}
+        />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Bejelentkezes />} />
+            <Route path="/admin/*" element={<Admin />} />
+          </Routes>
+        </Router>
+      )}
     </div>
   );
 }
