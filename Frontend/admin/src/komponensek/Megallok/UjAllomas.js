964affd6b4a515f50504a49c2be26b48c7218ca7
malackapite <veress.marcell@protonmail.com>
2024-04-04 20:02:25 +0200

    elso elem lesz mindig kivalasztva

--- Frontend/admin/src/komponensek/Megallok/UjAllomas.js
+++ Frontend/admin/src/komponensek/Megallok/UjAllomas.js
@@ -10,25 +10,33 @@ import React from "react";
 function UjAllomas(props){
 
     const {getAll} = useContext(AxiosContext)
-    const [nextElement, setNextElement] = React.useState(props.pool[0])
+    const [nextElement, setNextElement] = React.useState({... props.pool[0], ido: 1})
 
     //const [opcio, setOpciok] = React.useState(null)
-    const [adatok, setAdatok] = React.useState(null)
+    const [adatok, setAdatok] = React.useState(props.pool[0])
 
     const { pool, handleSave, name } = props;
 
     const handleChange = (event) => {
         const { name, value } = event.target;
-        console.log("asd", name, value);
+        // console.log("asd", name, value);
         setAdatok(values => ({ ...values, [name[0].toLowerCase() + name.slice(1)]: value }))
-        console.log(adatok, "ad");
+        // console.log(adatok, "ad");
     }
 
     useEffect(() => {   
+        setAdatok( (prev) => {
+            return {
+                ... props.pool[0]??null,
+                ...prev,
+            }}
+            )
         setNextElement(props.pool[0]??null)
     }, [props.pool[0]]);
    
     if (!props.pool) return null
+
+    console.log("ujallomas", props.pool, nextElement);
 
     return (
         props.pool.length==0
@@ -36,8 +44,8 @@ function UjAllomas(props){
         :
         <Row>
             <InputMezo as={Col} name="id" value="nev" pool={pool} isSelect={true} idk={true} handleChange={handleChange}/>
-            <InputMezo as={Col} input={{columnName: "ido", dataType: "tinyint"}} handleChange={handleChange}/>
-            <Button as={Col} variant="success" onClick={(event) => handleSave(event, adatok, name, handleChange, nextElement)}>Új állomás</Button>
+            <InputMezo as={Col} input={{columnName: "ido", dataType: "tinyint"}} value="1" handleChange={handleChange}/>
+            <Button as={Col} variant="success" onClick={(event) => handleSave(event, adatok, name, handleChange, nextElement)} style={{marginRight: "12px"}}>Új állomás</Button>
         </Row>
         
     );
