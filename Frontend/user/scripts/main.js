68c9fa9bd371be53d9f32f98558b2938f80ea9d3
Dark2Knight7 <ducpeti27@gmail.com>
2024-04-19 17:26:51 +0200

    mozgathato terkep

--- Frontend/user/scripts/main.js
+++ Frontend/user/scripts/main.js
@@ -7,6 +7,7 @@ function myFunction() {
         x.addClass("topnav");
     }
 }
+
 function togglePanel() {
     var panel = document.getElementById("panel");
     var toggleButtonIcon = document.getElementById("panelToggleButtonIcon");
@@ -20,8 +21,56 @@ function togglePanel() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.addEventListener("click", function(event) {
-        console.log("X koordináta: " + event.clientX + ", Y koordináta: " + event.clientY);
+$(document).ready(function () {
+    var x = 0;
+    var y = 0;
+    var img = $("#map");
+    var isDragging = false;
+
+    // This will run when the mouse button is pressed within the map container
+    img.on("mousedown", function (event) {
+        isDragging = true;
+        var offset = img.offset();
+        x = event.clientX - offset.left;
+        y = event.clientY - offset.top;
+    });
+
+    // This will run when the mouse button is released anywhere in the document
+    $(document).on("mouseup", function (event) {
+        if (isDragging) {
+            isDragging = false;
+        }
+    });
+
+    // This will run when the mouse is moved
+    $(document).on("mousemove", function (event) {
+        if (isDragging) {
+            var newX = event.clientX - x;
+            var newY = event.clientY - y;
+
+            // Limiting the movement within the bounds of the map container
+            var parentWidth = img.parent().width();
+            var parentHeight = img.parent().height();
+            var imgWidth = img.width();
+            var imgHeight = img.height();
+
+            if (newX > 0) {
+                newX = 0;
+            } else if (newX < parentWidth - imgWidth) {
+                newX = parentWidth - imgWidth;
+            }
+            if (newY > 0) {
+                newY = 0;
+            } else if (newY < parentHeight - imgHeight) {
+                newY = parentHeight - imgHeight;
+            }
+
+            img.css({left: newX + "px", top: newY + "px"});
+        }
+
+        // Preventing navigation when dragging near the bottom edge of the map
+        if (event.clientY > window.innerHeight - 50) {
+            event.preventDefault();
+        }
     });
 });
