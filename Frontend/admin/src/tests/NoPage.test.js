875f2dac29903af48cd28581b2f0bf4363691035
malackapite <veress.marcell@protonmail.com>
2024-04-20 14:33:53 +0200

    teszt leiras 📜

--- Frontend/admin/src/tests/NoPage.test.js
+++ Frontend/admin/src/tests/NoPage.test.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import NoPage from '../pages/Main/Admin/NoPage';
 
-it('renders correctly', () => {
+
+/**
+ * @function
+ * @name it
+ * @description Jest teszt függvény, ami ellenőrzi, hogy a NoPage komponens helyesen jelenik meg.
+ * Ellenőrzi, hogy a videó automatikusan indul-e, illetve folyamatosan játszdik-e le, valamint némítva van-e.
+ * Továbbá ellenőrzi, hogy a megfelelő videót jeleníti-e meg.
+ * @param {string} 'NoPage helyesen jelenik-e meg' - A teszt neve.
+ * @param {function} function - A tesztfüggvény.
+ * @callback
+ * @returns {void}
+ */
+it('NoPage helyesen jelenik-e meg', () => {
   const { getByTestId } = render(<NoPage />);
   const video = getByTestId('video');
 
