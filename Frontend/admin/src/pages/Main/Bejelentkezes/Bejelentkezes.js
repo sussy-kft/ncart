0348b5addaf7aefbd4d262b9a62722fba30a0aee
Dark2Knight7 <ducpeti27@gmail.com>
2024-04-27 15:52:30 +0200

    reszponzivitas fix + admin bejelentkezes hatter video

--- Frontend/admin/src/pages/Main/Bejelentkezes/Bejelentkezes.js
+++ Frontend/admin/src/pages/Main/Bejelentkezes/Bejelentkezes.js
@@ -1,103 +1,115 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import {Form} from "react-bootstrap";
 import InputMezo from "../../../komponensek/kozos/InputMezo";
 import Button from "react-bootstrap/Button";
-import { AxiosContext } from "../../../context/Alap/AxiosContext";
+import {AxiosContext} from "../../../context/Alap/AxiosContext";
 import "./Bejelentkezes.css";
-import { useNavigate } from "react-router-dom";
-import { DarkModeContext } from "../../../context/Alap/DarkModeContext";
-import { InfoPanelContext } from "../../../context/Alap/InfoPanelContext";
+import {useNavigate} from "react-router-dom";
+import {DarkModeContext} from "../../../context/Alap/DarkModeContext";
+import {InfoPanelContext} from "../../../context/Alap/InfoPanelContext";
+import videoBackground from "../../../media/map.mp4";
 
 /**
  * A Bejelentkezes komponens felelős a bejelentkezési űrlap megjelenítéséért és a bejelentkezési folyamat kezeléséért.
  * @returns {JSX.Element} A megjeleníteni kivánt `Bejelentkezes` komponenst.
  */
 function Bejelentkezes() {
-  const { getText } = React.useContext(DarkModeContext);
-  const { post } = React.useContext(AxiosContext);
-  const { resetInfoPanel } = React.useContext(InfoPanelContext);
+    const {getText} = React.useContext(DarkModeContext);
+    const {post} = React.useContext(AxiosContext);
+    const {resetInfoPanel} = React.useContext(InfoPanelContext);
 
-  const navigate = useNavigate();
-  const inputs = [
-    { columnName: "email", dataType: "email" },
-    { columnName: "password", dataType: "password", characterMinimumLength: 8 },
-  ];
+    const navigate = useNavigate();
+    const inputs = [
+        {columnName: "email", dataType: "email"},
+        {
+            columnName: "password",
+            dataType: "password",
+            characterMinimumLength: 8,
+        },
+    ];
 
-  const [validated, setValidated] = React.useState(false);
-  const [adatok, setAdatok] = React.useState({});
-  
-  /**
-   * Egy függvény, ami generál form input mezőket az inputs listából.
-   * @returns {Form.Group[]} Egy tömböt, ami a generált form input mezőket tartalmazza.
-   */
-  const inputGenerator = () => {
-    return (
-      inputs?.map((input) => {
+    const [validated, setValidated] = React.useState(false);
+    const [adatok, setAdatok] = React.useState({});
+
+    /**
+     * Egy függvény, ami generál form input mezőket az inputs listából.
+     * @returns {Form.Group[]} Egy tömböt, ami a generált form input mezőket tartalmazza.
+     */
+    const inputGenerator = () => {
         return (
-          <Form.Group className="mb-3" key={input.columnName}>
-            <Form.Label>{input.columnName + ": "}</Form.Label>
-            <InputMezo input={input} handleChange={handleChange} />
-            <Form.Control.Feedback type="invalid" />
-          </Form.Group>
+            inputs?.map((input) => {
+                return (
+                    <Form.Group className="mb-3" key={input.columnName}>
+                        <Form.Label>{input.columnName + ": "}</Form.Label>
+                        <InputMezo input={input} handleChange={handleChange} />
+                        <Form.Control.Feedback type="invalid" />
+                    </Form.Group>
+                );
+            }) || []
         );
-      }) || []
-    );
-  };
+    };
 
-  /**
-   * Egy függvény, ami a form adatait elküldi a backendnek, ami visszaküldi a token-t, amit a localStorage-ba menti.
-   * Továbbá a felhasználó email címét a sessionStorage-be menti, átirányítja az admin oldalra és törli az összes info panelt.
-   * @param {Event} event - A form küldési eseménye.
-   */
-  const handleSumbit = (event) => {
-    event.preventDefault();
-    setValidated(true);
-    post("kezelok/login", adatok, (valasz) => {
-      resetInfoPanel()
-      window.sessionStorage.setItem("felhasznalo", adatok.email);
-      localStorage.setItem("token", valasz.token);
-      navigate("/admin");
-    });
-  };
+    /**
+     * Egy függvény, ami a form adatait elküldi a backendnek, ami visszaküldi a token-t, amit a localStorage-ba menti.
+     * Továbbá a felhasználó email címét a sessionStorage-be menti, átirányítja az admin oldalra és törli az összes info panelt.
+     * @param {Event} event - A form küldési eseménye.
+     */
+    const handleSumbit = (event) => {
+        event.preventDefault();
+        setValidated(true);
+        post("kezelok/login", adatok, (valasz) => {
+            resetInfoPanel();
+            window.sessionStorage.setItem("felhasznalo", adatok.email);
+            localStorage.setItem("token", valasz.token);
+            navigate("/admin");
+        });
+    };
 
-  /**
-   * Egy függvény, ami a form input mezőinek változását kezeli.
-   * @param {Event} event - A megváltozott input mező eseménye.
-   */
-  const handleChange = (event) => {
-    setAdatok((values) => ({
-      ...values,
-      [event.target.name]: event.target.value,
-    }));
-  };
+    /**
+     * Egy függvény, ami a form input mezőinek változását kezeli.
+     * @param {Event} event - A megváltozott input mező eseménye.
+     */
+    const handleChange = (event) => {
+        setAdatok((values) => ({
+            ...values,
+            [event.target.name]: event.target.value,
+        }));
+    };
 
-  return (
-    <div
-      className="d-flex justify-content-center align-items-center"
-      style={{ height: "100vh" }}
-    >
-      <Form
-        noValidate
-        validated={validated}
-        id="bejelentkezes"
-        style={{
-          backgroundColor: getText() === "dark" ? "#1f2938" : "#f3f4f6",
-          color: getText() === "dark" ? "white" : "black",
-        }}
-        onSubmit={(event) => handleSumbit(event)}
-      >
-        {inputGenerator()}
-        <div className="d-flex justify-content-end mt-5">
-          <Button
-            type="submit"
-            variant={getText() === "dark" ? "light" : "dark"}
-          >
-            Bejelentkezés
-          </Button>
+    return (
+        <div className="video-background">
+            <video autoPlay loop muted className="fullscreen-bg__video">
+                <source src={videoBackground} type="video/mp4" />
+            </video>
+            <div
+                className="d-flex justify-content-center align-items-center"
+                style={{height: "100vh", position: "relative"}}
+            >
+                <Form
+                    noValidate
+                    validated={validated}
+                    id="bejelentkezes"
+                    style={{
+                        backgroundColor: "transparent", // Átlátszó háttér
+                        color: getText() === "dark" ? "white" : "black",
+                        position: "relative", // Hogy a form a videó felett legyen
+                        zIndex: 1, // Hogy a form a videó felett legyen
+                    }}
+                    onSubmit={(event) => handleSumbit(event)}
+                >
+                    {inputGenerator()}
+                    <div className="d-flex justify-content-end mt-5">
+                        <Button
+                            type="submit"
+                            variant={getText() === "dark" ? "light" : "dark"}
+                        >
+                            Bejelentkezés
+                        </Button>
+                    </div>
+                </Form>
+            </div>
         </div>
-      </Form>
-    </div>
-  );
+    );
 }
 
 export default Bejelentkezes;
